e325fb3a3a258d6c5409ffe25dddfea4b13fcb3d
JM Fantin <jmlerina@gmail.com>
2017-07-12 16:09:06 -0300

    class RelatedWords is now less personal (but needs fixing)

--- java/RelatedWords.java
+++ java/RelatedWords.java
@@ -6,36 +6,36 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class RelatedWords {
+public class RelatedWords { //builds lines to form bubble graphs.. still TBD
 
    public static void main(String args[])  throws IOException{
 		
-	    File inputFile = new File("C:\\Users\\eduardo\\Desktop\\coleta_carlos_11.05.txt");
-		File outputFile = new File("C:\\Users\\eduardo\\Desktop\\israel.txt");
+	    File inputFile = new File("C:\\Users\\...\\input.txt");
+		File outputFile = new File("C:\\Users\\...\\output.txt");
 		
 		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
 		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
 		int opt = 1;
 		Scanner in = new Scanner(System.in);
-		writer.write("termo1,termo2" + System.getProperty("line.separator"));
+		writer.write("term1,term2" + System.getProperty("line.separator"));
 		
 		while(opt != 0){
 			
-			System.out.println("Termo 1: ");
+			System.out.println("Term no1: ");
 			String t1 = in.next();
-			System.out.println("Entendi: " + t1);
-			System.out.println("Termo 2: ");
+			System.out.println("Got it.");
+			System.out.println("Term no2: ");
 			String t2 = in.next();
-			System.out.println("Entendi: " + t2);
+			System.out.println("Got it.");
 
 			String currentLine;
-			while((currentLine = reader.readLine()) != null) { //enquanto houver mais uma linha
+			while((currentLine = reader.readLine()) != null) {
 			    // trim newline when comparing with lineToRemove
 			    if(currentLine.toLowerCase().contains(t1) && currentLine.toLowerCase().contains(t2)){
 			    	writer.write(t1 +","+ t2 + System.getProperty("line.separator"));
 			    }
 			}
-			reader = new BufferedReader(new FileReader(inputFile)); //reseta a leitura
+			reader = new BufferedReader(new FileReader(inputFile)); //resets reading
 			
 		}
 		
