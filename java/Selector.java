b73bcaa43ef75b6465fff3bfb21b95fcd61a6a66
JM Fantin <jmlerina@gmail.com>
2017-07-12 16:32:34 -0300

    Selector class is simplified

--- java/Selector.java
+++ java/Selector.java
@@ -4,28 +4,24 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-/**
- * @version 1
- */
-public class Selector {//procura registros baseado no char inicial no ARQUIVO LIMPO, "_processado"
+
+public class Selector {
 	public static void main(String[]args) throws IOException{
-		File inputFile = new File("C:\\Users\\eduardo\\Music\\JM\\Coletas\\Lula vs Moro\\c3 graph content\\hour23.txt_processado.txt");
-		File tempFile = new File("C:\\Users\\eduardo\\Music\\JM\\Coletas\\Lula vs Moro\\c3 graph content\\hour23hashtags.txt");
+		File inputFile = new File("C:\\Users\\...\\processed_input.txt");
+		File tempFile = new File("C:\\Users\\...\\output.txt");
 
 		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
 		BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
 
-		String lineToSelect = "#"; 
 		String currentLine;
 
-		while((currentLine = reader.readLine()) != null) { //enquanto houver mais uma linha
+		while((currentLine = reader.readLine()) != null) {
 		    // trim newline when comparing with lineToRemove
-		    if(currentLine.contains(lineToSelect)){
-		    	writer.write(currentLine + System.getProperty("line.separator")); //é oq escreve no arquivo
+		    if(currentLine.contains("insert some string")){ //usually # or @
+		    	writer.write(currentLine + System.getProperty("line.separator"));
 		    }
 		}
 		writer.close(); 					
-		reader.close(); 
-		boolean successful = tempFile.renameTo(inputFile);
+		reader.close();
 	}
 }
